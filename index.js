a8eeed8556923cad1b58ffbf8aa2b825af58bd35
vinaymavi <vinaymavi@gmail.com>
2015-01-13 22:13:54 +0530

    first scrapping

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var request = require('request')
-  , cheerio = require('cheerio')
-  , async = require('async')
-  , format = require('util').format;
+  , cheerio = require('cheerio');  
 
-var reddits = [ 'programming', 'javascript', 'node' ]
-  , concurrency = 2;
+var URL = "https://developers.google.com/products/";
 
-async.eachLimit(reddits, concurrency, function (reddit, next) {
-    var url = format('http://reddit.com/r/%s', reddit);
-    request(url, function (err, response, body) {
-        if (err) throw err;
-        var $ = cheerio.load(body);
-        $('a.title').each(function () {
-            console.log('%s (%s)', $(this).text(), $(this).attr('href'));
-        });
-        next();
-    });
+request(URL,function(error,response,html){
+  // Error Handling
+  if(error){
+    console.log("Error Occur");
+  }
+
+  $=cheerio.load(html);
+  $('ul.alpha-sections>li').each(function(i,elem){
+    console.log($('div>h3',this).text());
+  }); 
 });
